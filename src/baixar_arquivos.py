ba325440cbbb3b777aa098bd8841b54638f65d32
LuizMiron <luiz.gugamiron@gmail.com>
2022-08-23 11:37:18 -0300

    complemento

--- src/baixar_arquivos.py
+++ src/baixar_arquivos.py
@@ -1,11 +1,16 @@
 import requests
+import os
 
 
 def baixar_arquivo(url, endereco):
     # faz requisição ao servidor
     resposta = requests.get(url)
-    with open(endereco, 'wb') as novo_arquivo:
-        novo_arquivo.write(resposta.content)
+    if resposta.status_code == requests.codes.OK:
+        with open(endereco, 'wb') as novo_arquivo:
+            novo_arquivo.write(resposta.content)
+        print(f'Download finalizado Salvo em: {os.path.abspath(endereco)}')
+    else:
+        resposta.raise_for_status()
 
 
 if __name__ == '__main__':
